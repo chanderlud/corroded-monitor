590b4499d3b9fcb474e0ee3da317ca4a3fa4d551
chand <chanchan@sirchanchan.dev>
2023-05-12 15:09:47 -0700

    add icon resource

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 
 use bindgen;
 use embed_resource;
+use winres;
 
 fn main() {
     println!("cargo:rustc-link-search=native=LibreHardwareMonitorAPI\\x64\\Release");
@@ -22,5 +23,9 @@ fn main() {
 
     // forces app to run as admin
     embed_resource::compile("program.rc", embed_resource::NONE);
+
+    winres::WindowsResource::new()
+        .set_icon("icon.ico")
+        .compile().expect("failed to add icon to executable");
 }
 
