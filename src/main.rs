2a100d879d7b4bcc29cfe60d0f023c22f84b0ab0
chand <chanchan@sirchanchan.dev>
2023-05-12 11:53:36 -0700

    disables console for non debug builds only

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-#![windows_subsystem = "windows"]
+// disables console for non debug builds only
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
 use system::{cpu, gpu, ram, storage};
+
+// block non windows builds
+#[cfg(not(target_os = "windows"))]
+compile_error!("This application only supports Windows.");
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -12,7 +18,6 @@ mod system;
 mod config;
 
 // TODO optional stats, dont render them if they arent available
-// TODO try to setup auto into theme for styles
 // TODO scrollable is crashing shit i think (github issue)
 fn main() {
     if let Err(error) = ui::main() {
